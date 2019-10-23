73d0c707044c21243ff0a764750ed8ba5978754a
rloops <xramov.xramov@gmail.com>
2019-10-23 11:27:32 +0400

    Add Comments with MongoDB

--- server/server.js
+++ server/server.js
@@ -5,8 +5,82 @@ import bodyParser from 'body-parser';
 import path from 'path';
 import IntlWrapper from '../client/modules/Intl/IntlWrapper';
 
+import logger from 'morgan';
+import { getSecret } from './secrets';
+import Comment from './models/comment';
+
 // Initialize the Express App
 const app = new Express();
+const router = Express.Router();
+
+mongoose.connect(getSecret('dbUri'));
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(logger('dev'));
+
+router.get('/', (req, res) => {
+  res.json({ message: 'Hello, World!' });
+});
+
+router.get('/comments', (req, res) => {
+  Comment.find((err, comments) => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true, data: comments });
+  });
+});
+
+router.post('/comments', (req, res) => {
+  const comment = new Comment();
+  // body parser lets us use the req.body
+  const { author, text } = req.body;
+  if (!author || !text) {
+    // we should throw an error. we can do this check on the front end
+    return res.json({
+      success: false,
+      error: 'You must provide an author and comment'
+    });
+  }
+  comment.author = author;
+  comment.text = text;
+  comment.save(err => {
+    if (err) return res.json({ success: false, error: err });
+    return res.json({ success: true });
+  });
+});
+
+router.put('/comments/:commentId', (req, res) => {
+  console.log(req.params);
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.findById(commentId, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    const { author, text } = req.body;
+    if (author) comment.author = author;
+    if (text) comment.text = text;
+    comment.save(error => {
+      if (error) return res.json({ success: false, error });
+      return res.json({ success: true });
+    });
+  });
+});
+
+router.delete('/comments/:commentId', (req, res) => {
+  const { commentId } = req.params;
+  if (!commentId) {
+    return res.json({ success: false, error: 'No comment id provided' });
+  }
+  Comment.remove({ _id: commentId }, (error, comment) => {
+    if (error) return res.json({ success: false, error });
+    return res.json({ success: true });
+  });
+});
+
+app.use('/api', router);
 
 // Set Development modes checks
 const isDevMode = process.env.NODE_ENV === 'development' || false;
@@ -92,7 +166,7 @@ const renderFullPage = (html, initialState) => {
 
         ${isProdMode ? `<link rel='stylesheet' href='${assetsManifest['/app.css']}' />` : ''}
         <link href='https://fonts.googleapis.com/css?family=Lato:400,300,700' rel='stylesheet' type='text/css'/>
-        <link rel="shortcut icon" href="http://res.cloudinary.com/hashnode/image/upload/v1455629445/static_imgs/mern/mern-favicon-circle-fill.png" type="image/png" />
+        <link rel="shortcut icon" href="https://raw.githubusercontent.com/IBM-Cloud/MERN-app/master/public/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         <div id="root">${process.env.NODE_ENV === 'production' ? html : `<div>${html}</div>`}</div>
